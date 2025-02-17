f411d024f74d9453be9054c9f4139611c1a9a53c
houxizhu <houxizhu@gmail.com>
2025-02-18 00:43:14 +1100

    leetcode/easy/1716-calculate-money-in-leetcode-bank.py

---
+++ leetcode/easy/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,64 @@
+"""
+1716. Calculate Money in Leetcode Bank
+Solved
+Easy
+Topics
+Companies
+Hint
+Hercy wants to save money for his first car. He puts money in the Leetcode bank every day.
+
+He starts by putting in $1 on Monday, the first day. Every day from Tuesday to Sunday, he will put in $1 more than the day before. On every subsequent Monday, he will put in $1 more than the previous Monday.
+
+Given n, return the total amount of money he will have in the Leetcode bank at the end of the nth day.
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: 10
+Explanation: After the 4th day, the total is 1 + 2 + 3 + 4 = 10.
+Example 2:
+
+Input: n = 10
+Output: 37
+Explanation: After the 10th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4) = 37. Notice that on the 2nd Monday, Hercy only puts in $2.
+Example 3:
+
+Input: n = 20
+Output: 96
+Explanation: After the 20th day, the total is (1 + 2 + 3 + 4 + 5 + 6 + 7) + (2 + 3 + 4 + 5 + 6 + 7 + 8) + (3 + 4 + 5 + 6 + 7 + 8) = 96.
+ 
+
+Constraints:
+
+1 <= n <= 1000
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        result = 0
+        weeks = n//7
+        days = n%7
+
+        result += weeks*28 + (weeks*(weeks-1)//2)*7
+        result += weeks*days + (days*(days+1)//2)
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
