d434ca8411562e05abe84f993880966ab2322a8e
houxizhu <houxizhu@gmail.com>
2025-02-17 15:11:21 +1100

    leetcode, easy/1710-maximum-units-on-a-truck.py

---
+++ leetcode/medium/151-reverse-words-in-a-string.py
@@ -0,0 +1,73 @@
+"""
+151. Reverse Words in a String
+Solved
+Medium
+Topics
+Companies
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> str:
+        s += " "
+        words = []
+        word = ""
+        for each in s:
+            if each == " ":
+                if len(word) > 0:
+                    words.append(word)
+                    word = ""
+                continue
+            word += each
+        words = words[::-1]
+        return " ".join(words)
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
