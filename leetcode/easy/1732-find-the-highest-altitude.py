4924f437b778a5125778bac028f664d16501696f
houxizhu <houxizhu@gmail.com>
2025-02-19 00:01:17 +1100

    leetcode/easy/1732-find-the-highest-altitude.py

---
+++ leetcode/easy/1732-find-the-highest-altitude.py
@@ -0,0 +1,56 @@
+"""
+1732. Find the Highest Altitude
+Easy
+Topics
+Companies
+Hint
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+ 
+
+Example 1:
+
+Input: gain = [-5,1,5,0,-7]
+Output: 1
+Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+Example 2:
+
+Input: gain = [-4,-3,-2,-1,4,3,2]
+Output: 0
+Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+ 
+
+Constraints:
+
+n == gain.length
+1 <= n <= 100
+-100 <= gain[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, gain: List[int]) -> int:
+        n = len(gain)
+        for ii in range(1,n):
+            gain[ii] += gain[ii-1]
+            #print(gain)
+
+        return max(gain+[0])
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
