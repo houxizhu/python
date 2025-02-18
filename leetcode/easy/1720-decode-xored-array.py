20fada78b4b76597c061fbf82632a478eef115ee
houxizhu <houxizhu@gmail.com>
2025-02-18 23:12:06 +1100

    leetcode/easy/1720-decode-xored-array.py

---
+++ leetcode/easy/1720-decode-xored-array.py
@@ -0,0 +1,61 @@
+"""
+1720. Decode XORed Array
+Solved
+Easy
+Topics
+Companies
+Hint
+There is a hidden integer array arr that consists of n non-negative integers.
+
+It was encoded into another integer array encoded of length n - 1, such that encoded[i] = arr[i] XOR arr[i + 1]. For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+
+You are given the encoded array. You are also given an integer first, that is the first element of arr, i.e. arr[0].
+
+Return the original array arr. It can be proved that the answer exists and is unique.
+
+ 
+
+Example 1:
+
+Input: encoded = [1,2,3], first = 1
+Output: [1,0,2,1]
+Explanation: If arr = [1,0,2,1], then first = 1 and encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+Example 2:
+
+Input: encoded = [6,2,7,3], first = 4
+Output: [4,2,0,7,4]
+ 
+
+Constraints:
+
+2 <= n <= 104
+encoded.length == n - 1
+0 <= encoded[i] <= 105
+0 <= first <= 105
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, encoded: List[int], first: int) -> List[int]:
+        encoded = [first] + encoded
+        ll = len(encoded)
+        for ii in range(1,ll):
+            encoded[ii] ^= encoded[ii-1]
+
+        return encoded
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
