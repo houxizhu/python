70a87064736ce108d63b3bc5dc4ba59d0de3ea79
houxizhu <houxizhu@gmail.com>
2025-02-19 22:02:31 +1100

    codeforeces 174

--- codeforces/edu.174/e.py
+++ codeforces/edu.174/e.py
@@ -1,7 +1,83 @@
 # -*- coding: utf-8 -*-
 
 """
+E. A, B, AB and BA
+time limit per test2 seconds
+memory limit per test256 megabytes
+You are given a string s
+ consisting of characters A and B.
 
+Your task is to split it into blocks of length 1
+ and 2
+ in such a way that
+
+there are no more than a
+ strings equal to "A";
+there are no more than b
+ strings equal to "B";
+there are no more than ab
+ strings "AB";
+there are no more than ba
+ strings "BA";
+Strings "AA" and "BB" are prohibited. Each character of the initial string s
+ should belong to exactly one block.
+
+Input
+The first line contains one integer t
+ (1≤t≤104
+) — the number of test cases. Next, t
+ independent cases follow.
+
+The first line of each test case contains a single string s
+ (1≤|s|≤5⋅105
+) consisting only of characters A and/or B.
+
+The second line of each test case contains four integers a
+, b
+, ab
+, and ba
+ (0≤a,b,ab,ba≤5⋅105
+) — the maximum allowed number of strings "A", "B", "AB", and "BA" correspondingly.
+
+It's guaranteed that the total length of s
+ doesn't exceed 5⋅105
+ over all test cases.
+
+Output
+For each test case, print YES if it's possible to split string s
+. Otherwise, print NO.
+
+Example
+InputCopy
+7
+A
+0 0 10 10
+B
+0 1 0 0
+ABA
+0 0 1 1
+ABBABAAB
+5 5 0 0
+ABABBAABBAAB
+1 1 2 3
+ABBBBAB
+0 3 2 0
+BAABBA
+1 3 2 0
+OutputCopy
+NO
+YES
+NO
+YES
+YES
+YES
+NO
+Note
+In the third test case, all possible splits are: A|B|A, AB|A or A|BA. All of them have at least one "A".
+
+In the fourth test case, one of the possible splits is the following: A|B|B|A|B|A|A|B.
+
+In the fifth test case, one of the possible splits is the following: A|BA|B|BA|AB|BA|AB
 """
 
 import string
--- codeforces/edu.174/f.py
+++ codeforces/edu.174/f.py
@@ -1,6 +1,92 @@
 # -*- coding: utf-8 -*-
 
 """
+F. Graph Inclusion
+time limit per test5 seconds
+memory limit per test512 megabytes
+A connected component of an undirected graph is defined as a set of vertices S
+ of this graph such that:
+
+for every pair of vertices (u,v)
+ in S
+, there exists a path between vertices u
+ and v
+;
+there is no vertex outside S
+ that has a path to a vertex within S
+.
+For example, the graph in the picture below has three components: {1,3,7,8}
+, {2}
+, {4,5,6}
+.
+
+
+We say that graph A
+ includes graph B
+ if every component of graph B
+ is a subset of some component of graph A
+.
+
+You are given two graphs, A
+ and B
+, both consisting of n
+ vertices numbered from 1
+ to n
+. Initially, there are no edges in the graphs. You must process queries of two types:
+
+add an edge to one of the graphs;
+remove an edge from one of the graphs.
+After each query, you have to calculate the minimum number of edges that have to be added to A
+ so that A
+ includes B
+, and print it. Note that you don't actually add these edges, you just calculate their number.
+
+Input
+The first line contains two integers n
+ and q
+ (2≤n≤4⋅105
+; 1≤q≤4⋅105
+) — the number of vertices and queries, respectively.
+
+Next, there are q
+ lines, where the i
+-th line describes the i
+-th query. The description of the query begins with the character ci
+ (either A or B) — the graph to which the query should be applied. Then, two integers xi
+ and yi
+ follow (1≤xi,yi≤n
+; xi≠yi
+). If there is an edge (xi,yi)
+ in the corresponding graph, it should be removed; otherwise, it should be added to that graph.
+
+Output
+For each query, print one integer — the minimum number of edges that you have to add to the graph A
+ so that it includes B
+.
+
+Example
+InputCopy
+6 9
+A 2 3
+B 1 3
+A 2 1
+A 3 2
+B 5 6
+A 6 5
+A 3 4
+A 4 2
+A 4 3
+OutputCopy
+0
+1
+0
+1
+2
+1
+1
+0
+1
+
 
 """
 
