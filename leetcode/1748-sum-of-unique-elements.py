8404318ba93faed682da688413eda8d88ab09e1c
houxizhu <houxizhu@gmail.com>
2025-02-21 01:54:07 +1100

    leetcode/1748-sum-of-unique-elements.py

---
+++ leetcode/1748-sum-of-unique-elements.py
@@ -0,0 +1,64 @@
+"""
+1748. Sum of Unique Elements
+Easy
+Topics
+Companies
+Hint
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        dd = defaultdict(int)
+        for each in nums:
+            dd[each] += 1
+
+        result = 0
+        for each in nums:
+            if dd[each] == 1:
+                result += each
+                
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
