14393fd3fb1afb7488795dc6b6f00452161891ca
houxizhu <houxizhu@gmail.com>
2025-02-24 01:39:37 +1100

    leetcode, 150.11, leetcode/medium/274-h-index.py

---
+++ leetcode/medium/274-h-index.py
@@ -0,0 +1,59 @@
+"""
+274. H-Index
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+ 
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+ 
+
+Constraints:
+
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, citations: List[int]) -> int:
+        ll = len(citations)
+        result = 0
+        citations.sort(reverse=True)
+        for ii in range(ll):
+            current = min(ii+1,citations[ii])
+            result = max(result, current)
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
