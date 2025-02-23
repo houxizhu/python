cbac467c74f0731fad8d9d533965a02fa7e0e1ec
houxizhu <houxizhu@gmail.com>
2025-02-24 02:05:56 +1100

    leetcode.75.13, leetcode/medium/1679-max-number-k-sum-pairs.py

---
+++ leetcode/medium/1679-max-number-k-sum-pairs.py
@@ -0,0 +1,78 @@
+"""
+1679. Max Number of K-Sum Pairs
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int], k: int) -> int:
+        result = 0
+        ll = len(nums)
+        nums.sort()
+        start = 0
+        end = ll-1
+        while start < end:
+            if nums[start]+nums[end] == k:
+                result += 1
+                start += 1
+                end -= 1
+            elif nums[start] >= k:
+                break
+            elif nums[start]+nums[end] > k:
+                end -= 1
+            elif nums[start]+nums[end] < k:
+                start += 1
+            else:
+                end -= 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
