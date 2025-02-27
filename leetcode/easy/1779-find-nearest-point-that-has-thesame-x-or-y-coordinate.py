bdb4f34c683ee87da14edb31bb2914f644c8587c
houxizhu <houxizhu@gmail.com>
2025-02-27 12:48:03 +1100

    leetcode/easy/1779-find-nearest-point-that-has-thesame-x-or-y-coordinate.py

---
+++ leetcode/easy/1779-find-nearest-point-that-has-thesame-x-or-y-coordinate.py
@@ -0,0 +1,70 @@
+"""
+1779. Find Nearest Point That Has the Same X or Y Coordinate
+Easy
+Topics
+Companies
+Hint
+You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y). You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
+
+Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location. If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
+
+The Manhattan distance between two points (x1, y1) and (x2, y2) is abs(x1 - x2) + abs(y1 - y2).
+
+ 
+
+Example 1:
+
+Input: x = 3, y = 4, points = [[1,2],[3,1],[2,4],[2,3],[4,4]]
+Output: 2
+Explanation: Of all the points, only [3,1], [2,4] and [4,4] are valid. Of the valid points, [2,4] and [4,4] have the smallest Manhattan distance from your current location, with a distance of 1. [2,4] has the smallest index, so return 2.
+Example 2:
+
+Input: x = 3, y = 4, points = [[3,4]]
+Output: 0
+Explanation: The answer is allowed to be on the same location as your current location.
+Example 3:
+
+Input: x = 3, y = 4, points = [[2,3]]
+Output: -1
+Explanation: There are no valid points.
+ 
+
+Constraints:
+
+1 <= points.length <= 104
+points[i].length == 2
+1 <= x, y, ai, bi <= 104
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, x: int, y: int, points: List[List[int]]) -> int:
+        mdistance = 1000000
+        result = -1
+        ll = len(points)
+        for ii in range(ll):
+            a,b = points[ii]
+            if x == a and abs(y-b) < mdistance:
+                mdistance = abs(y-b)
+                result = ii
+            elif y == b and abs(x-a) < mdistance:
+                mdistance = abs(x-a)
+                result = ii
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
