ae15897d5ed5184680f87bd0c0e79a7f985d7edc
houxizhu <houxizhu@gmail.com>
2025-03-02 14:57:01 +1100

    leetcode/contest/20250302.weekly.439

---
+++ leetcode/contest/20250302.weekly.439/q2-.py
@@ -0,0 +1,65 @@
+"""
+Q2. Longest Palindromic Subsequence After at Most K Operations
+Medium
+4 pt.
+You are given a string s and an integer k.
+
+In one operation, you can replace the character at any position with the next or previous letter in the alphabet (wrapping around so that 'a' is after 'z'). For example, replacing 'a' with the next letter results in 'b', and replacing 'a' with the previous letter results in 'z'. Similarly, replacing 'z' with the next letter results in 'a', and replacing 'z' with the previous letter results in 'y'.
+
+Return the length of the longest palindromic subsequence of s that can be obtained after performing at most k operations.
+
+A subsequence is a non-empty string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+A palindrome is a string that reads the same forward and backward.
+
+ 
+
+Example 1:
+
+Input: s = "abced", k = 2
+
+Output: 3
+
+Explanation:
+
+Replace s[1] with the next letter, and s becomes "acced".
+Replace s[4] with the previous letter, and s becomes "accec".
+The subsequence "ccc" forms a palindrome of length 3, which is the maximum.
+
+Example 2:
+
+Input: s = "aaazzz", k = 4
+
+Output: 6
+
+Explanation:
+
+Replace s[0] with the previous letter, and s becomes "zaazzz".
+Replace s[4] with the next letter, and s becomes "zaazaz".
+Replace s[3] with the next letter, and s becomes "zaaaaz".
+The entire string forms a palindrome of length 6.
+
+ 
+
+Constraints:
+
+1 <= s.length <= 200
+1 <= k <= 200
+s consists of only lowercase English letters.
+"""
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        result = 0
+
+        return result
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [1,4,3,3,2]
+    b = 2
+    print(app.leetcode(a))
