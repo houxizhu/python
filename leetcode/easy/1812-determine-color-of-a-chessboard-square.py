e39ce101943e0d61c25075963ea635d48d083540
houxizhu <houxizhu@gmail.com>
2025-03-03 12:24:58 +1100

    leetcode/easy/1812-determine-color-of-a-chessboard-square.py

---
+++ leetcode/easy/1812-determine-color-of-a-chessboard-square.py
@@ -0,0 +1,65 @@
+"""
+1812. Determine Color of a Chessboard Square
+Easy
+Topics
+Companies
+Hint
+You are given coordinates, a string that represents the coordinates of a square of the chessboard. Below is a chessboard for your reference.
+
+
+
+Return true if the square is white, and false if the square is black.
+
+The coordinate will always represent a valid chessboard square. The coordinate will always have the letter first, and the number second.
+
+ 
+
+Example 1:
+
+Input: coordinates = "a1"
+Output: false
+Explanation: From the chessboard above, the square with coordinates "a1" is black, so return false.
+Example 2:
+
+Input: coordinates = "h3"
+Output: true
+Explanation: From the chessboard above, the square with coordinates "h3" is white, so return true.
+Example 3:
+
+Input: coordinates = "c7"
+Output: false
+ 
+
+Constraints:
+
+coordinates.length == 2
+'a' <= coordinates[0] <= 'h'
+'1' <= coordinates[1] <= '8'
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, coordinates: str) -> bool:
+        a,n = coordinates
+        if a in "aceg" and n in "1357":
+            return False
+        if a in "bdfh" and n in "2468":
+            return False
+
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
