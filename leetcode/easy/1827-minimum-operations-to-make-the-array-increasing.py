9d698f27cfd642c1f16a4ac8d5b05621a74bbbfa
houxizhu <houxizhu@gmail.com>
2025-03-05 13:06:42 +1100

    leetcode/easy/1827-minimum-operations-to-make-the-array-increasing.py

---
+++ leetcode/easy/1827-minimum-operations-to-make-the-array-increasing.py
@@ -0,0 +1,66 @@
+"""
+1827. Minimum Operations to Make the Array Increasing
+Easy
+Topics
+Companies
+Hint
+You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+
+For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+Return the minimum number of operations needed to make nums strictly increasing.
+
+An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1]
+Output: 3
+Explanation: You can do the following operations:
+1) Increment nums[2], so nums becomes [1,1,2].
+2) Increment nums[1], so nums becomes [1,2,2].
+3) Increment nums[2], so nums becomes [1,2,3].
+Example 2:
+
+Input: nums = [1,5,2,4,1]
+Output: 14
+Example 3:
+
+Input: nums = [8]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+1 <= nums[i] <= 104
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        result = 0
+        for ii in range(1,ll):
+            if nums[ii] > nums[ii-1]:
+                continue
+            result += nums[ii-1] - nums[ii]+1
+            nums[ii] += nums[ii-1] - nums[ii]+1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
