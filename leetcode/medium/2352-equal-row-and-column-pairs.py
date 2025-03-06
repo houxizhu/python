19abfabe358bb68acc19f97f026a80e2b0a101b7
houxizhu <houxizhu@gmail.com>
2025-03-06 13:21:47 +1100

    leetcode/medium/2352-equal-row-and-column-pairs.py

---
+++ leetcode/medium/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,67 @@
+"""
+2352. Equal Row and Column Pairs
+Medium
+Topics
+Companies
+Hint
+Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+
+A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+
+ 
+
+Example 1:
+
+
+Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+Output: 1
+Explanation: There is 1 equal row and column pair:
+- (Row 2, Column 1): [2,7,7]
+Example 2:
+
+
+Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+Output: 3
+Explanation: There are 3 equal row and column pairs:
+- (Row 0, Column 0): [3,1,2,2]
+- (Row 2, Column 2): [2,4,2,2]
+- (Row 3, Column 2): [2,4,2,2]
+ 
+
+Constraints:
+
+n == grid.length == grid[i].length
+1 <= n <= 200
+1 <= grid[i][j] <= 105
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        result = 0
+        for rr in range(n):
+            for cc in range(n):
+                equal_pair = 1
+                for ii in range(n):
+                    if grid[rr][ii] != grid[ii][cc]:
+                        equal_pair = 0
+                        break
+                result += equal_pair
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
