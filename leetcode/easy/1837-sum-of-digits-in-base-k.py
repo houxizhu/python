45a50c722d97325f1b6f69ce611b6cea6bf40d9f
houxizhu <houxizhu@gmail.com>
2025-03-07 00:02:39 +1100

    leetcode/easy/1837-sum-of-digits-in-base-k.py

---
+++ leetcode/easy/1837-sum-of-digits-in-base-k.py
@@ -0,0 +1,55 @@
+"""
+1837. Sum of Digits in Base K
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+
+After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+
+ 
+
+Example 1:
+
+Input: n = 34, k = 6
+Output: 9
+Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
+Example 2:
+
+Input: n = 10, k = 10
+Output: 1
+Explanation: n is already in base 10. 1 + 0 = 1.
+ 
+
+Constraints:
+
+1 <= n <= 100
+2 <= k <= 10
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int, k: int) -> int:
+        result = 0
+        while n:
+            result += n%k
+            n //= k
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
