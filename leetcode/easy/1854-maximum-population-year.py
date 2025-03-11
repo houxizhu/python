2431208df4342e0c76a76ee6449a1d235716e815
houxizhu <houxizhu@gmail.com>
2025-03-11 11:32:07 +1100

    leetcode/easy/1854-maximum-population-year.py

---
+++ leetcode/easy/1854-maximum-population-year.py
@@ -0,0 +1,70 @@
+"""
+1854. Maximum Population Year
+Easy
+Topics
+Companies
+Hint
+You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
+
+The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
+
+Return the earliest year with the maximum population.
+
+ 
+
+Example 1:
+
+Input: logs = [[1993,1999],[2000,2010]]
+Output: 1993
+Explanation: The maximum population is 1, and 1993 is the earliest year with this population.
+Example 2:
+
+Input: logs = [[1950,1961],[1960,1971],[1970,1981]]
+Output: 1960
+Explanation: 
+The maximum population is 2, and it had happened in years 1960 and 1970.
+The earlier year between them is 1960.
+ 
+
+Constraints:
+
+1 <= logs.length <= 100
+1950 <= birthi < deathi <= 2050
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, logs: List[List[int]]) -> int:
+        result = 0
+        population = 0
+        max_population = 0
+        q = []
+        for birth,death in logs:
+            q.append([birth,1])
+            q.append([death,-1])
+        
+        q.sort()
+        ll = len(q)
+        for ii in range(ll):
+            population += q[ii][1]
+            if population > max_population:
+                max_population = population
+                result = q[ii][0]
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
