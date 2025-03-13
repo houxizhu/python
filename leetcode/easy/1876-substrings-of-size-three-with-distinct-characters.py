4cfeb20455ef618b15e6cc515f1f49d32029d447
houxizhu <houxizhu@gmail.com>
2025-03-14 00:52:35 +1100

    leetcode/easy/1876-substrings-of-size-three-with-distinct-characters.py

---
+++ leetcode/easy/1876-substrings-of-size-three-with-distinct-characters.py
@@ -0,0 +1,66 @@
+"""
+1876. Substrings of Size Three with Distinct Characters
+Easy
+Topics
+Companies
+Hint
+A string is good if there are no repeated characters.
+
+Given a string s​​​​​, return the number of good substrings of length three in s​​​​​​.
+
+Note that if there are multiple occurrences of the same substring, every occurrence should be counted.
+
+A substring is a contiguous sequence of characters in a string.
+
+ 
+
+Example 1:
+
+Input: s = "xyzzaz"
+Output: 1
+Explanation: There are 4 substrings of size 3: "xyz", "yzz", "zza", and "zaz". 
+The only good substring of length 3 is "xyz".
+Example 2:
+
+Input: s = "aababcabc"
+Output: 4
+Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc".
+The good substrings are "abc", "bca", "cab", and "abc".
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s​​​​​​ consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        ll = len(s)
+        result = 0
+        for ii in range(ll-2):
+            if s[ii] == s[ii+1]:
+                continue
+            if s[ii] == s[ii+2]:
+                continue
+            if s[ii+1] == s[ii+2]:
+                continue
+            result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
