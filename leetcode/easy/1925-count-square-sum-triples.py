014d144baf67b40bd50603d24c4f145821d8b4b7
houxizhu <houxizhu@gmail.com>
2025-03-23 09:25:03 +1100

    leetcode/easy/1925-count-square-sum-triples.py

---
+++ leetcode/easy/1925-count-square-sum-triples.py
@@ -0,0 +1,56 @@
+"""
+1925. Count Square Sum Triples
+Easy
+Topics
+Companies
+Hint
+A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
+
+Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 2
+Explanation: The square triples are (3,4,5) and (4,3,5).
+Example 2:
+
+Input: n = 10
+Output: 4
+Explanation: The square triples are (3,4,5), (4,3,5), (6,8,10), and (8,6,10).
+ 
+
+Constraints:
+
+1 <= n <= 250
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        square = [ii*ii for ii in range(1, n+1)]
+        result = 0
+        for ii in range(1,n+1):
+            for jj in range(1,n+1):
+                if ii*ii+jj*jj in square:
+                    #print(ii,jj)
+                    result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
