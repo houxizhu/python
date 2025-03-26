41ddd3e1c071a277a508ef07143924ea85db3630
houxizhu <houxizhu@gmail.com>
2025-03-26 13:23:42 +1100

    easy/1941-check-if-all-characters-have-equal-number-of-occurrences.py

---
+++ leetcode/easy/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -0,0 +1,57 @@
+"""
+1941. Check if All Characters Have Equal Number of Occurrences
+Easy
+Topics
+Companies
+Hint
+Given a string s, return true if s is a good string, or false otherwise.
+
+A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+ 
+
+Example 1:
+
+Input: s = "abacbc"
+Output: true
+Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+Example 2:
+
+Input: s = "aaabb"
+Output: false
+Explanation: The characters that appear in s are 'a' and 'b'.
+'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> bool:
+        dd = defaultdict(int)
+        for each in s:
+            dd[each] += 1
+        for each in dd:
+            if dd[each] != dd[s[0]]:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
