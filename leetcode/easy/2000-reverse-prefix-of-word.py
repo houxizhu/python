49d18470bd64e1a60a9192703170b6e6be6ea7a1
houxizhu <houxizhu@gmail.com>
2025-04-10 21:34:43 +1000

    leetcode/easy/2000-reverse-prefix-of-word.py

---
+++ leetcode/easy/2000-reverse-prefix-of-word.py
@@ -0,0 +1,69 @@
+"""
+2000. Reverse Prefix of Word
+Solved
+Easy
+Topics
+Companies
+Hint
+Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+Return the resulting string.
+
+ 
+
+Example 1:
+
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3. 
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+Example 2:
+
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+Example 3:
+
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word.
+You should not do any reverse operation, the resulting string is "abcd".
+ 
+
+Constraints:
+
+1 <= word.length <= 250
+word consists of lowercase English letters.
+ch is a lowercase English letter.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, word: str, ch: str) -> str:
+        q = ""
+        ll = len(word)
+        for ii in range(ll):
+            if word[ii] != ch:
+                q = word[ii]+q
+            else:
+                return ch+q+word[ii+1:]
+
+        return word
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
