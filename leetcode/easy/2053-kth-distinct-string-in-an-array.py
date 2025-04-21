534933d06ded6b3ccea908e328620b4adb42212e
houxizhu <houxizhu@gmail.com>
2025-04-22 05:36:57 +1000

    leetcode/easy/2053-kth-distinct-string-in-an-array.py

---
+++ leetcode/easy/2053-kth-distinct-string-in-an-array.py
@@ -0,0 +1,72 @@
+"""
+2053. Kth Distinct String in an Array
+
+A distinct string is a string that is present only once in an array.
+
+Given an array of strings arr, and an integer k, return the kth distinct string present in arr. If there are fewer than k distinct strings, return an empty string "".
+
+Note that the strings are considered in the order in which they appear in the array.
+
+ 
+
+Example 1:
+
+Input: arr = ["d","b","c","b","c","a"], k = 2
+Output: "a"
+Explanation:
+The only distinct strings in arr are "d" and "a".
+"d" appears 1st, so it is the 1st distinct string.
+"a" appears 2nd, so it is the 2nd distinct string.
+Since k == 2, "a" is returned. 
+Example 2:
+
+Input: arr = ["aaa","aa","a"], k = 1
+Output: "aaa"
+Explanation:
+All strings in arr are distinct, so the 1st string "aaa" is returned.
+Example 3:
+
+Input: arr = ["a","b","a"], k = 3
+Output: ""
+Explanation:
+The only distinct string is "b". Since there are fewer than 3 distinct strings, we return an empty string "".
+ 
+
+Constraints:
+
+1 <= k <= arr.length <= 1000
+1 <= arr[i].length <= 5
+arr[i] consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, arr: List[str], k: int) -> str:
+        dd = defaultdict(int)
+        for each in arr:
+            dd[each] += 1
+        index = 0
+        ll = len(arr)
+        while index < ll:
+            if dd[arr[index]] == 1:
+                k -= 1
+                if k == 0:
+                    return arr[index]
+            index += 1
+        return ""
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
