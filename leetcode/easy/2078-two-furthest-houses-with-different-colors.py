544ff3906548cf9d998b0ee147442a053b6b8f33
houxizhu <houxizhu@gmail.com>
2025-04-30 00:59:01 +1000

    leetcode/easy/2078-two-furthest-houses-with-different-colors.py

---
+++ leetcode/easy/2078-two-furthest-houses-with-different-colors.py
@@ -0,0 +1,79 @@
+"""
+2078. Two Furthest Houses With Different Colors
+
+There are n houses evenly lined up on the street, and each house is beautifully painted. You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+
+Return the maximum distance between two houses with different colors.
+
+The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+
+ 
+
+Example 1:
+
+
+Input: colors = [1,1,1,6,1,1,1]
+Output: 3
+Explanation: In the above image, color 1 is blue, and color 6 is red.
+The furthest two houses with different colors are house 0 and house 3.
+House 0 has color 1, and house 3 has color 6. The distance between them is abs(0 - 3) = 3.
+Note that houses 3 and 6 can also produce the optimal answer.
+Example 2:
+
+
+Input: colors = [1,8,3,8,3]
+Output: 4
+Explanation: In the above image, color 1 is blue, color 8 is yellow, and color 3 is green.
+The furthest two houses with different colors are house 0 and house 4.
+House 0 has color 1, and house 4 has color 3. The distance between them is abs(0 - 4) = 4.
+Example 3:
+
+Input: colors = [0,1]
+Output: 1
+Explanation: The furthest two houses with different colors are house 0 and house 1.
+House 0 has color 0, and house 1 has color 1. The distance between them is abs(0 - 1) = 1.
+ 
+
+Constraints:
+
+n == colors.length
+2 <= n <= 100
+0 <= colors[i] <= 100
+Test data are generated such that at least two houses have different colors.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, colors: List[int]) -> int:
+        ll = len(colors)
+        if colors[0] != colors[ll-1]:
+            return ll-1
+        ddsmall = defaultdict(int)
+        ddbig = defaultdict(int)
+        for ii in range(ll):
+            if colors[ii] not in ddsmall:
+                ddsmall[colors[ii]] = ii
+            ddbig[colors[ii]] = ii
+        small_index = list(ddsmall.values())
+        small_index.sort()
+        big_index = list(ddbig.values())
+        big_index.sort()
+        result = max(big_index[-2]-small_index[0], big_index[-1]-small_index[1])
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
