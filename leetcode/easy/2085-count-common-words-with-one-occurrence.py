062484689bc683f43df386151d3ab77a7b33c738
Xizhu Hou <houxizhu@gmail.com>
2025-05-01 14:32:55 +1000

    leetcode/easy/2085-count-common-words-with-one-occurrence.py

---
+++ leetcode/easy/2085-count-common-words-with-one-occurrence.py
@@ -0,0 +1,69 @@
+"""
+2085. Count Common Words With One Occurrence
+Easy
+Topics
+Companies
+Hint
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+ 
+
+Example 1:
+
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+Example 2:
+
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+Example 3:
+
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+ 
+
+Constraints:
+
+1 <= words1.length, words2.length <= 1000
+1 <= words1[i].length, words2[j].length <= 30
+words1[i] and words2[j] consists only of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, words1: List[str], words2: List[str]) -> int:
+        dd1 = defaultdict(int)
+        dd2 = defaultdict(int)
+        for each in words1:
+            dd1[each] += 1
+        for each in words2:
+            dd2[each] += 1
+        result = 0
+        for each in dd1:
+            if dd1[each] == 1 and dd2[each] == 1:
+                result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
