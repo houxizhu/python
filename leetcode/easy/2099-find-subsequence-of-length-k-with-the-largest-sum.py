4f1610276af9e61db3f84afa528f4cc71d5d7ccc
Xizhu Hou <houxizhu@gmail.com>
2025-05-05 00:10:25 +1000

    leetcode/easy/2099-find-subsequence-of-length-k-with-the-largest-sum.py

---
+++ leetcode/easy/2099-find-subsequence-of-length-k-with-the-largest-sum.py
@@ -0,0 +1,67 @@
+"""
+2099. Find Subsequence of Length K With the Largest Sum
+
+You are given an integer array nums and an integer k. You want to find a subsequence of nums of length k that has the largest sum.
+
+Return any such subsequence as an integer array of length k.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,3,3], k = 2
+Output: [3,3]
+Explanation:
+The subsequence has the largest sum of 3 + 3 = 6.
+Example 2:
+
+Input: nums = [-1,-2,3,4], k = 3
+Output: [-1,3,4]
+Explanation: 
+The subsequence has the largest sum of -1 + 3 + 4 = 6.
+Example 3:
+
+Input: nums = [3,4,3,3], k = 2
+Output: [3,4]
+Explanation:
+The subsequence has the largest sum of 3 + 4 = 7. 
+Another possible subsequence is [4, 3].
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-105 <= nums[i] <= 105
+1 <= k <= nums.length
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int], k: int) -> List[int]:
+        ll = len(nums)
+        nums1 = [[ii,num] for ii,num in enumerate(nums)]
+        nums1.sort(reverse=True,key=lambda x:x[1])
+        nums2 = sorted(nums1[0:k])
+        nums2.sort()
+        
+        result = [num for _,num in nums2]
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
