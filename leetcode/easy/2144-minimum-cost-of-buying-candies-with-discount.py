e31bbddc610d40919ec8478ab165bc50bf6101b7
Xizhu Hou <houxizhu@gmail.com>
2025-05-13 00:07:21 +1000

    leetcode/easy/2144-minimum-cost-of-buying-candies-with-discount.py

---
+++ leetcode/easy/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,75 @@
+"""
+2144. Minimum Cost of Buying Candies With Discount
+Easy
+Topics
+Companies
+Hint
+A shop is selling candies at a discount. For every two candies sold, the shop gives a third candy for free.
+
+The customer can choose any candy to take away for free as long as the cost of the chosen candy is less than or equal to the minimum cost of the two candies bought.
+
+For example, if there are 4 candies with costs 1, 2, 3, and 4, and the customer buys candies with costs 2 and 3, they can take the candy with cost 1 for free, but not the candy with cost 4.
+Given a 0-indexed integer array cost, where cost[i] denotes the cost of the ith candy, return the minimum cost of buying all the candies.
+
+ 
+
+Example 1:
+
+Input: cost = [1,2,3]
+Output: 5
+Explanation: We buy the candies with costs 2 and 3, and take the candy with cost 1 for free.
+The total cost of buying all candies is 2 + 3 = 5. This is the only way we can buy the candies.
+Note that we cannot buy candies with costs 1 and 3, and then take the candy with cost 2 for free.
+The cost of the free candy has to be less than or equal to the minimum cost of the purchased candies.
+Example 2:
+
+Input: cost = [6,5,7,9,2,2]
+Output: 23
+Explanation: The way in which we can get the minimum cost is described below:
+- Buy candies with costs 9 and 7
+- Take the candy with cost 6 for free
+- We buy candies with costs 5 and 2
+- Take the last remaining candy with cost 2 for free
+Hence, the minimum cost to buy all candies is 9 + 7 + 5 + 2 = 23.
+Example 3:
+
+Input: cost = [5,5]
+Output: 10
+Explanation: Since there are only 2 candies, we buy both of them. There is not a third candy we can take for free.
+Hence, the minimum cost to buy all candies is 5 + 5 = 10.
+ 
+
+Constraints:
+
+1 <= cost.length <= 100
+1 <= cost[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, cost: List[int]) -> int:
+        result = sum(cost)
+        ll = len(cost)
+        cost.sort()
+        while ll%3 > 0:
+            cost.pop(0)
+            ll -= 1
+        for ii in range(ll//3):
+            result -= cost[ii*3]
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
