2e5c5ccf847eea69d7442f2dc65c49e444e9fdd7
Xizhu Hou <houxizhu@gmail.com>
2025-05-14 09:55:44 +1000

    leetcode/easy/2148-count-elements-with-strictly-smaller-and-greater.py

---
+++ leetcode/easy/2148-count-elements-with-strictly-smaller-and-greater.py
@@ -0,0 +1,59 @@
+"""
+2148. Count Elements With Strictly Smaller and Greater Elements 
+Easy
+Topics
+Companies
+Hint
+Given an integer array nums, return the number of elements that have both a strictly smaller and a strictly greater element appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [11,7,2,15]
+Output: 2
+Explanation: The element 7 has the element 2 strictly smaller than it and the element 11 strictly greater than it.
+Element 11 has element 7 strictly smaller than it and element 15 strictly greater than it.
+In total there are 2 elements having both a strictly smaller and a strictly greater element appear in nums.
+Example 2:
+
+Input: nums = [-3,3,3,90]
+Output: 2
+Explanation: The element 3 has the element -3 strictly smaller than it and the element 90 strictly greater than it.
+Since there are two elements with the value 3, in total there are 2 elements having both a strictly smaller and a strictly greater element appear in nums.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-105 <= nums[i] <= 105
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        result = len(nums)
+        min_nums = min(nums)
+        max_nums = max(nums)
+        for each in nums:
+            if each == min_nums:
+                result -= 1
+            elif each == max_nums:
+                result -= 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
