fc7e7a5c4857e2beebb535449e241521e5036cf5
Xizhu Hou <houxizhu@gmail.com>
2025-05-18 11:39:12 +1000

    leetcode/easy/2180-count-integers-with-even-digit-sum.py

---
+++ leetcode/easy/2180-count-integers-with-even-digit-sum.py
@@ -0,0 +1,66 @@
+"""
+2180. Count Integers With Even Digit Sum
+Easy
+Topics
+Companies
+Hint
+Given a positive integer num, return the number of positive integers less than or equal to num whose digit sums are even.
+
+The digit sum of a positive integer is the sum of all its digits.
+
+ 
+
+Example 1:
+
+Input: num = 4
+Output: 2
+Explanation:
+The only integers less than or equal to 4 whose digit sums are even are 2 and 4.    
+Example 2:
+
+Input: num = 30
+Output: 14
+Explanation:
+The 14 integers less than or equal to 30 whose digit sums are even are
+2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24, 26, and 28.
+ 
+
+Constraints:
+
+1 <= num <= 1000
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, num: int) -> int:
+        if num <= 10:
+            return num//2
+        
+        if num == 1000:
+            return 499
+
+        result = 4
+        result += (num//10-1)*5
+
+        ab = num//100 + num%100//10
+        for c in range(num%10+1):
+            if (ab+c)%2 == 0:
+                result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
