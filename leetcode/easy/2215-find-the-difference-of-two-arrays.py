8ecc80a630ffd74f8e0133177c3fd8dabb69ff91
Xizhu Hou <houxizhu@gmail.com>
2025-05-25 10:32:37 +1000

    leetcode/easy/2215-find-the-difference-of-two-arrays.py

---
+++ leetcode/easy/2215-find-the-difference-of-two-arrays.py
@@ -0,0 +1,68 @@
+"""
+2215. Find the Difference of Two Arrays
+Solved
+Easy
+Topics
+Companies
+Hint
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3], nums2 = [2,4,6]
+Output: [[1,3],[4,6]]
+Explanation:
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+Example 2:
+
+Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+Output: [[3],[]]
+Explanation:
+For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+-1000 <= nums1[i], nums2[i] <= 1000
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        ll1 = len(nums1)
+        ll2 = len(nums2)
+        answer0 = []
+        answer1 = []
+        for each in nums1:
+            if each not in nums2 and each not in answer0:
+                answer0.append(each)
+        for each in nums2:
+            if each not in nums1  and each not in answer1:
+                answer1.append(each)
+        result = [answer0, answer1]
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
