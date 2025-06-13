d5639c8f663a0d7be65069c8044c4181d76cf5b6
Xizhu Hou <houxizhu@gmail.com>
2025-06-13 12:13:45 +1000

    leetcode/easy/2315-count-asterisks.py

---
+++ leetcode/easy/2315-count-asterisks.py
@@ -0,0 +1,69 @@
+"""
+2315. Count Asterisks
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair. In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+
+Return the number of '*' in s, excluding the '*' between each pair of '|'.
+
+Note that each '|' will belong to exactly one pair.
+
+ 
+
+Example 1:
+
+Input: s = "l|*e*et|c**o|*de|"
+Output: 2
+Explanation: The considered characters are underlined: "l|*e*et|c**o|*de|".
+The characters between the first and second '|' are excluded from the answer.
+Also, the characters between the third and fourth '|' are excluded from the answer.
+There are 2 asterisks considered. Therefore, we return 2.
+Example 2:
+
+Input: s = "iamprogrammer"
+Output: 0
+Explanation: In this example, there are no asterisks in s. Therefore, we return 0.
+Example 3:
+
+Input: s = "yo|uar|e**|b|e***au|tifu|l"
+Output: 5
+Explanation: The considered characters are underlined: "yo|uar|e**|b|e***au|tifu|l". There are 5 asterisks considered. Therefore, we return 5.
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters, vertical bars '|', and asterisks '*'.
+s contains an even number of vertical bars '|'.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        flag = True
+        result = 0
+        for each in s:
+            if each == "|":
+                flag = not flag
+            if each == "*" and flag:
+                result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
