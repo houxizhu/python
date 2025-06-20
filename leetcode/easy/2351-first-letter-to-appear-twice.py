51da8d936c89ea0afe0075ecd9f39904465c1cc1
Xizhu Hou <houxizhu@gmail.com>
2025-06-20 10:10:41 +1000

    leetcode/easy/2351-first-letter-to-appear-twice.py

---
+++ leetcode/easy/2351-first-letter-to-appear-twice.py
@@ -0,0 +1,64 @@
+"""
+2351. First Letter to Appear Twice
+Easy
+Topics
+Companies
+Hint
+Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+
+Note:
+
+A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+s will contain at least one letter that appears twice.
+ 
+
+Example 1:
+
+Input: s = "abccbaacz"
+Output: "c"
+Explanation:
+The letter 'a' appears on the indexes 0, 5 and 6.
+The letter 'b' appears on the indexes 1 and 4.
+The letter 'c' appears on the indexes 2, 3 and 7.
+The letter 'z' appears on the index 8.
+The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
+Example 2:
+
+Input: s = "abcdd"
+Output: "d"
+Explanation:
+The only letter that appears twice is 'd' so we return 'd'.
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists of lowercase English letters.
+s has at least one repeated letter.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> str:
+        dd = defaultdict(int)
+        for each in s:
+            dd[each] += 1
+            if dd[each] > 1:
+                return each
+        return ""
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
