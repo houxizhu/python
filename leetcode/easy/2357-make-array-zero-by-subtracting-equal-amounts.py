548bbcec3f0fd8f2454f26442175010ac7600690
Xizhu Hou <houxizhu@gmail.com>
2025-06-21 12:25:07 +1000

    leetcode/easy/2357-make-array-zero-by-subtracting-equal-amounts.py

---
+++ leetcode/easy/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,62 @@
+"""
+2357. Make Array Zero by Subtracting Equal Amounts
+Easy
+Topics
+Companies
+Hint
+You are given a non-negative integer array nums. In one operation, you must:
+
+Choose a positive integer x such that x is less than or equal to the smallest non-zero element in nums.
+Subtract x from every positive element in nums.
+Return the minimum number of operations to make every element in nums equal to 0.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,0,3,5]
+Output: 3
+Explanation:
+In the first operation, choose x = 1. Now, nums = [0,4,0,2,4].
+In the second operation, choose x = 2. Now, nums = [0,2,0,0,2].
+In the third operation, choose x = 2. Now, nums = [0,0,0,0,0].
+Example 2:
+
+Input: nums = [0]
+Output: 0
+Explanation: Each element in nums is already 0 so no operations are needed.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        dd = defaultdict(int)
+        for each in nums:
+            if each > 0:
+                dd[each] += 1
+        result = 0
+        for each in dd:
+            result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
