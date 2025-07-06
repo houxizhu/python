ff2fd852068fd3c0be8fe2ea66f15a3f38bf1be3
Xizhu Hou <houxizhu@gmail.com>
2025-07-07 09:57:28 +1000

    leetcode/easy/2441.py

---
+++ leetcode/easy/2441.py
@@ -0,0 +1,73 @@
+"""
+2441. Largest Positive Integer That Exists With Its Negative
+Easy
+Topics
+Companies
+Hint
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+Return the positive integer k. If there is no such integer, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,-3,3]
+Output: 3
+Explanation: 3 is the only valid k we can find in the array.
+Example 2:
+
+Input: nums = [-1,10,6,7,-7,1]
+Output: 7
+Explanation: Both 1 and 7 have their corresponding negative values in the array. 7 has a larger value.
+Example 3:
+
+Input: nums = [-10,8,6,7,-2,-3]
+Output: -1
+Explanation: There is no a single valid k, we return -1.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+nums[i] != 0
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        ll = len(nums)
+        indexp = ll-1
+        indexn = 0
+        while indexn < indexp:
+            np = nums[indexn] + nums[indexp]
+            if np == 0:
+                return nums[indexp]
+            elif np > 0:
+                indexp -= 1
+            else:
+                indexn += 1
+
+        return -1
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
