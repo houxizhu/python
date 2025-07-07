ecb5e6e6740bb8c36610264db2b8eb6f88348a3e
Xizhu Hou <houxizhu@gmail.com>
2025-07-08 00:49:32 +1000

    leetcode/easy/2446.py

---
+++ leetcode/easy/2446.py
@@ -0,0 +1,77 @@
+"""
+2446. Determine if Two Events Have Conflict
+Easy
+Topics
+Companies
+Hint
+You are given two arrays of strings that represent two inclusive events that happened on the same day, event1 and event2, where:
+
+event1 = [startTime1, endTime1] and
+event2 = [startTime2, endTime2].
+Event times are valid 24 hours format in the form of HH:MM.
+
+A conflict happens when two events have some non-empty intersection (i.e., some moment is common to both events).
+
+Return true if there is a conflict between two events. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: event1 = ["01:15","02:00"], event2 = ["02:00","03:00"]
+Output: true
+Explanation: The two events intersect at time 2:00.
+Example 2:
+
+Input: event1 = ["01:00","02:00"], event2 = ["01:20","03:00"]
+Output: true
+Explanation: The two events intersect starting from 01:20 to 02:00.
+Example 3:
+
+Input: event1 = ["10:00","11:00"], event2 = ["14:00","15:00"]
+Output: false
+Explanation: The two events do not intersect.
+ 
+
+Constraints:
+
+event1.length == event2.length == 2
+event1[i].length == event2[i].length == 5
+startTime1 <= endTime1
+startTime2 <= endTime2
+All the event times follow the HH:MM format.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, event1, event2):
+        """
+        :type event1: List[str]
+        :type event2: List[str]
+        :rtype: bool
+        """
+        if event1[1] == event2[0]:
+            return True
+        if event1[0] == event2[1]:
+            return True
+        if sorted(event1+event2)[0:2] == event1:
+            return False
+        if sorted(event1+event2)[0:2] == event2:
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
