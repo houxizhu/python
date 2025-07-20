3b691f3bb0442f5479dc31f2a5224afad48ebfe6
Xizhu Hou <houxizhu@gmail.com>
2025-07-20 11:03:44 +1000

    leetcode/easy/2506.py

---
+++ leetcode/easy/2506.py
@@ -0,0 +1,77 @@
+"""
+2506. Count Pairs Of Similar Strings
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a 0-indexed string array words.
+
+Two strings are similar if they consist of the same characters.
+
+For example, "abca" and "cba" are similar since both consist of characters 'a', 'b', and 'c'.
+However, "abacba" and "bcfd" are not similar since they do not consist of the same characters.
+Return the number of pairs (i, j) such that 0 <= i < j <= word.length - 1 and the two strings words[i] and words[j] are similar.
+
+ 
+
+Example 1:
+
+Input: words = ["aba","aabb","abcd","bac","aabc"]
+Output: 2
+Explanation: There are 2 pairs that satisfy the conditions:
+- i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'. 
+- i = 3 and j = 4 : both words[3] and words[4] only consist of characters 'a', 'b', and 'c'. 
+Example 2:
+
+Input: words = ["aabb","ab","ba"]
+Output: 3
+Explanation: There are 3 pairs that satisfy the conditions:
+- i = 0 and j = 1 : both words[0] and words[1] only consist of characters 'a' and 'b'. 
+- i = 0 and j = 2 : both words[0] and words[2] only consist of characters 'a' and 'b'.
+- i = 1 and j = 2 : both words[1] and words[2] only consist of characters 'a' and 'b'.
+Example 3:
+
+Input: words = ["nba","cba","dba"]
+Output: 0
+Explanation: Since there does not exist any pair that satisfies the conditions, we return 0.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consist of only lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, words: List[str]) -> int:
+        ll = len(words)
+        for ii in range(ll):
+            words[ii] = "".join(sorted(list(set(list(words[ii])))))
+        dd = defaultdict(int)
+        for each in words:
+            dd[each] += 1
+        result = 0
+        print(dd)
+        for each in dd:
+            result += dd[each]*(dd[each]-1)//2
+            print(result)
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
