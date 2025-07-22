956544c5fa258d090258325046e689823af5507e
Xizhu Hou <houxizhu@gmail.com>
2025-07-23 00:01:25 +1000

    leetcode/easy/2525.py

---
+++ leetcode/easy/2525.py
@@ -0,0 +1,74 @@
+"""
+2525. Categorize Box According to Criteria
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given four integers length, width, height, and mass, representing the dimensions and mass of a box, respectively, return a string representing the category of the box.
+
+The box is "Bulky" if:
+Any of the dimensions of the box is greater or equal to 104.
+Or, the volume of the box is greater or equal to 109.
+If the mass of the box is greater or equal to 100, it is "Heavy".
+If the box is both "Bulky" and "Heavy", then its category is "Both".
+If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+Note that the volume of the box is the product of its length, width and height.
+
+ 
+
+Example 1:
+
+Input: length = 1000, width = 35, height = 700, mass = 300
+Output: "Heavy"
+Explanation: 
+None of the dimensions of the box is greater or equal to 104. 
+Its volume = 24500000 <= 109. So it cannot be categorized as "Bulky".
+However mass >= 100, so the box is "Heavy".
+Since the box is not "Bulky" but "Heavy", we return "Heavy".
+Example 2:
+
+Input: length = 200, width = 50, height = 800, mass = 50
+Output: "Neither"
+Explanation: 
+None of the dimensions of the box is greater or equal to 104.
+Its volume = 8 * 106 <= 109. So it cannot be categorized as "Bulky".
+Its mass is also less than 100, so it cannot be categorized as "Heavy" either. 
+Since its neither of the two above categories, we return "Neither".
+ 
+
+Constraints:
+
+1 <= length, width, height <= 105
+1 <= mass <= 103
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, length: int, width: int, height: int, mass: int) -> str:
+        volume = length*width*height
+        if length >= 10000 or width >= 10000 or height >= 10000 or volume >= 10**9:
+            if mass >= 100:
+                return "Both"
+            return "Bulky"
+        if mass >= 100:
+            return "Heavy"
+        return "Neither"
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
