1b95724b6644b997218de9f3299f1c48594a3819
Xizhu Hou <houxizhu@gmail.com>
2025-07-27 10:31:13 +1000

    leetcode/easy/2544.py

---
+++ leetcode/easy/2544.py
@@ -0,0 +1,66 @@
+"""
+2544. Alternating Digit Sum
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a positive integer n. Each digit of n has a sign according to the following rules:
+
+The most significant digit is assigned a positive sign.
+Each other digit has an opposite sign to its adjacent digits.
+Return the sum of all digits with their corresponding sign.
+
+ 
+
+Example 1:
+
+Input: n = 521
+Output: 4
+Explanation: (+5) + (-2) + (+1) = 4.
+Example 2:
+
+Input: n = 111
+Output: 1
+Explanation: (+1) + (-1) + (+1) = 1.
+Example 3:
+
+Input: n = 886996
+Output: 0
+Explanation: (+8) + (-8) + (+6) + (-9) + (+9) + (-6) = 0.
+ 
+
+Constraints:
+
+1 <= n <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        flag = 0
+        result = 0
+        for ii in range(9,-1,-1):
+            if flag == 0 and n//(10**ii) > 0:
+                flag = 1
+            result += n//(10**ii)*flag
+            n %= 10**ii
+            flag *= -1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
