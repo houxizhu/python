3015dbe2e64febaa74f34ce129dff6cc980cec5a
Xizhu Hou <houxizhu@gmail.com>
2025-08-11 14:15:25 +1000

    leetcode/medium/2438.py

---
+++ leetcode/medium/2438.py
@@ -0,0 +1,72 @@
+"""
+2438. Range Product Queries of Powers
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a positive integer n, there exists a 0-indexed array called powers, composed of the minimum number of powers of 2 that sum to n. The array is sorted in non-decreasing order, and there is only one way to form the array.
+
+You are also given a 0-indexed 2D integer array queries, where queries[i] = [lefti, righti]. Each queries[i] represents a query where you have to find the product of all powers[j] with lefti <= j <= righti.
+
+Return an array answers, equal in length to queries, where answers[i] is the answer to the ith query. Since the answer to the ith query may be too large, each answers[i] should be returned modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: n = 15, queries = [[0,1],[2,2],[0,3]]
+Output: [2,4,64]
+Explanation:
+For n = 15, powers = [1,2,4,8]. It can be shown that powers cannot be a smaller size.
+Answer to 1st query: powers[0] * powers[1] = 1 * 2 = 2.
+Answer to 2nd query: powers[2] = 4.
+Answer to 3rd query: powers[0] * powers[1] * powers[2] * powers[3] = 1 * 2 * 4 * 8 = 64.
+Each answer modulo 109 + 7 yields the same answer, so [2,4,64] is returned.
+Example 2:
+
+Input: n = 2, queries = [[0,0]]
+Output: [2]
+Explanation:
+For n = 2, powers = [2].
+The answer to the only query is powers[0] = 2. The answer modulo 109 + 7 is the same, so [2] is returned.
+ 
+
+Constraints:
+
+1 <= n <= 109
+1 <= queries.length <= 105
+0 <= starti <= endi < powers.length
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int, queries: List[List[int]]) -> List[int]:
+        powers = []
+        for ii in range(32):
+            if n & (1 << ii) > 0:
+                powers.append(ii)
+        ll = len(queries)
+        result = [0]*ll
+        for ii in range(ll):
+            left, right = queries[ii]
+            sumlr = sum(powers[left:right+1])
+            result[ii] = (2**sumlr)%1000000007
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
