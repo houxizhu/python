3dc38ed3e650a1f63ba988dc1310d0e3bbfa8071
Xizhu Hou <houxizhu@gmail.com>
2025-08-19 11:00:02 +1000

    leetcode/medium/2348.py

---
+++ leetcode/medium/2348.py
@@ -0,0 +1,75 @@
+"""
+2348. Number of Zero-Filled Subarrays
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, return the number of subarrays filled with 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,0,0,2,0,0,4]
+Output: 6
+Explanation: 
+There are 4 occurrences of [0] as a subarray.
+There are 2 occurrences of [0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 2 filled with 0. Therefore, we return 6.
+Example 2:
+
+Input: nums = [0,0,0,2,0,0]
+Output: 9
+Explanation:
+There are 5 occurrences of [0] as a subarray.
+There are 3 occurrences of [0,0] as a subarray.
+There is 1 occurrence of [0,0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 3 filled with 0. Therefore, we return 9.
+Example 3:
+
+Input: nums = [2,10,2019]
+Output: 0
+Explanation: There is no subarray filled with 0. Therefore, we return 0.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        nums.append(1)
+        ll = len(nums)
+        result = 0
+        count = 0
+        for ii in range(ll):
+            if nums[ii] == 0:
+                count += 1
+            else:
+                result += count*(count+1)//2
+                count = 0
+
+        return result
+
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
