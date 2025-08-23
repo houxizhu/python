7e7acee29401b5c76da495a66b6b4d03f19d0208
Xizhu Hou <houxizhu@gmail.com>
2025-08-23 11:42:17 +1000

    150, leetcode/medium/167.py

---
+++ leetcode/medium/167.py
@@ -0,0 +1,72 @@
+"""
+7. Two Sum II - Input Array Is Sorted
+Solved
+Medium
+Topics
+￼
+Companies
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, numbers: List[int], target: int) -> List[int]:
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i1 in range(len(numbers)):
+            if numbers[i1] > numbers[i1-1] or i1 == 0:
+                if target-numbers[i1] in numbers[i1+1:]:
+                    return [i1+1, numbers[i1+1:].index(target-numbers[i1])+(i1+1)+1]
+        return None
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
