fc82041ae49ed579ce335a9fe431f079b03e27f5
Xizhu Hou <houxizhu@gmail.com>
2025-08-26 00:02:42 +1000

    leetcode/easy/2696.py

---
+++ leetcode/easy/2696.py
@@ -0,0 +1,69 @@
+"""
+2696. Minimum String Length After Removing Substrings
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a string s consisting only of uppercase English letters.
+
+You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+Return the minimum possible length of the resulting string that you can obtain.
+
+Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+ 
+
+Example 1:
+
+Input: s = "ABFCACDB"
+Output: 2
+Explanation: We can do the following operations:
+- Remove the substring "ABFCACDB", so s = "FCACDB".
+- Remove the substring "FCACDB", so s = "FCAB".
+- Remove the substring "FCAB", so s = "FC".
+So the resulting length of the string is 2.
+It can be shown that it is the minimum length that we can obtain.
+Example 2:
+
+Input: s = "ACBBD"
+Output: 5
+Explanation: We cannot do any operations on the string so the length remains the same.
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists only of uppercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        q = ["a"]
+        for each in s:
+            if each == "B" and q[-1] == "A":
+                q.pop()
+                continue
+            if each == "D" and q[-1] == "C":
+                q.pop()
+                continue
+            q.append(each)
+
+        return len(q)-1
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
