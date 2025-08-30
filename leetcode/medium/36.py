a99f13dd182f1584a39d276e06a9340dc7c5fe03
Xizhu Hou <houxizhu@gmail.com>
2025-08-30 12:39:50 +1000

    leetcode/medium/36.py, daily

---
+++ leetcode/medium/36.py
@@ -0,0 +1,98 @@
+"""
+36. Valid Sudoku
+Medium
+Topics
+￼
+Companies
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+ 
+
+Example 1:
+
+￼
+Input: board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board = 
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, board: List[List[str]]) -> bool:
+        squares = [defaultdict(int) for _ in range(9)]
+        rows = [defaultdict(int) for _ in range(9)]
+        columns = [defaultdict(int) for _ in range(9)]
+
+        for rr in range(9):
+            for cc in range(9):
+                if board[rr][cc] == "":
+                    continue
+                no_square = (rr//3)*3+(cc//3)
+                squares[no_square][board[rr][cc]] += 1
+                rows[rr][board[rr][cc]] += 1
+                columns[cc][board[rr][cc]] += 1
+        
+        #print(squares)
+
+        for num in "123456789":
+            for ii in range(9):
+                #print(squares[ii][num])
+                if squares[ii][num] > 1:
+                    return False
+                if rows[ii][num] > 1:
+                    return False
+                if columns[ii][num] > 1:
+                    return False
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
