9f779569834346316f6d5a716b615722aacef6af
Xizhu Hou <houxizhu@gmail.com>
2025-09-01 16:50:23 +1000

    75, leetcode/medium/2336.py

---
+++ leetcode/medium/2336.py
@@ -0,0 +1,88 @@
+"""
+2336. Smallest Number in Infinite Set
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You have a set which contains all positive integers [1, 2, 3, 4, 5, ...].
+
+Implement the SmallestInfiniteSet class:
+
+SmallestInfiniteSet() Initializes the SmallestInfiniteSet object to contain all positive integers.
+int popSmallest() Removes and returns the smallest integer contained in the infinite set.
+void addBack(int num) Adds a positive integer num back into the infinite set, if it is not already in the infinite set.
+ 
+
+Example 1:
+
+Input
+["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+[[], [2], [], [], [], [1], [], [], []]
+Output
+[null, null, 1, 2, 3, null, 1, 4, 5]
+
+Explanation
+SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+                                   // is the smallest number, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+ 
+
+Constraints:
+
+1 <= num <= 1000
+At most 1000 calls will be made in total to popSmallest and addBack.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.popped = [0]
+        heapq.heapify(self.popped)
+        self.current = 1
+        
+
+    def popSmallest(self) -> int:
+        result = self.current
+        heapq.heappush(self.popped, self.current)
+        while self.current in self.popped:
+            self.current += 1
+        return result
+        
+
+    def addBack(self, num: int) -> None:
+        if num in self.popped:
+            self.popped.remove(num)
+            heapq.heapify(self.popped)
+            if num < self.current:
+                self.current = num
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
