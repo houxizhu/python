b9e82099f5349f42e35104602b8c824e8d5b0677
Xizhu Hou <houxizhu@gmail.com>
2025-09-01 17:01:04 +1000

    75, leetcode/medium/2542.py

---
+++ leetcode/medium/2542.py
@@ -0,0 +1,83 @@
+"""
+2542. Maximum Subsequence Score
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given two 0-indexed integer arrays nums1 and nums2 of equal length n and a positive integer k. You must choose a subsequence of indices from nums1 of length k.
+
+For chosen indices i0, i1, ..., ik - 1, your score is defined as:
+
+The sum of the selected elements from nums1 multiplied with the minimum of the selected elements from nums2.
+It can defined simply as: (nums1[i0] + nums1[i1] +...+ nums1[ik - 1]) * min(nums2[i0] , nums2[i1], ... ,nums2[ik - 1]).
+Return the maximum possible score.
+
+A subsequence of indices of an array is a set that can be derived from the set {0, 1, ..., n-1} by deleting some or no elements.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,3,3,2], nums2 = [2,1,3,4], k = 3
+Output: 12
+Explanation: 
+The four possible subsequence scores are:
+- We choose the indices 0, 1, and 2 with score = (1+3+3) * min(2,1,3) = 7.
+- We choose the indices 0, 1, and 3 with score = (1+3+2) * min(2,1,4) = 6. 
+- We choose the indices 0, 2, and 3 with score = (1+3+2) * min(2,3,4) = 12. 
+- We choose the indices 1, 2, and 3 with score = (3+3+2) * min(1,3,4) = 8.
+Therefore, we return the max score, which is 12.
+Example 2:
+
+Input: nums1 = [4,2,3,1,1], nums2 = [7,5,10,9,6], k = 1
+Output: 30
+Explanation: 
+Choosing index 2 is optimal: nums1[2] * nums2[2] = 3 * 10 = 30 is the maximum possible score.
+ 
+
+Constraints:
+
+n == nums1.length == nums2.length
+1 <= n <= 105
+0 <= nums1[i], nums2[j] <= 105
+1 <= k <= n
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        pairs = sorted(zip(nums2, nums1), reverse=True)  # sort by nums2 desc
+        heap = []   # min-heap for nums1
+        total = 0
+        ans = 0
+
+        for n2, n1 in pairs:
+            heapq.heappush(heap, n1)
+            total += n1
+
+            # if heap grows beyond k, remove smallest
+            if len(heap) > k:
+                total -= heapq.heappop(heap)
+
+            # valid subsequence of size k
+            if len(heap) == k:
+                ans = max(ans, total * n2)
+
+        return ans
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
