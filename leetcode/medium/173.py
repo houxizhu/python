cad6cbe97b82b6b8c8865d0f6aae79a7969c6352
Xizhu Hou <houxizhu@gmail.com>
2025-09-01 22:17:05 +1000

    150, leetcode/medium/173.py

---
+++ leetcode/medium/173.py
@@ -0,0 +1,103 @@
+"""
+173. Binary Search Tree Iterator
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+ 
+
+Example 1:
+
+
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 105].
+0 <= Node.val <= 106
+At most 105 calls will be made to hasNext, and next.
+ 
+
+Follow up:
+
+Could you implement next() and hasNext() to run in average O(1) time and use O(h) memory, where h is the height of the tree?
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.iter = []
+        if root == None:
+            return
+        q = [root]
+        while len(q):
+            c = q.pop()
+            self.iter.append(c.val)
+            if c.left:
+                q.append(c.left)
+            if c.right:
+                q.append(c.right)
+        self.iter.append(min(self.iter)-1)
+        self.iter.sort()
+        self.idx = 0
+        
+
+    def next(self) -> int:
+        self.idx += 1
+        return self.iter[self.idx]
+
+    def hasNext(self) -> bool:
+        if self.idx >= len(self.iter)-1:
+            return False
+        return True
+        
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
