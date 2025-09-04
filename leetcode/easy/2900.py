4eef04962f6fd76199921598c37ceec02b948d48
Xizhu Hou <houxizhu@gmail.com>
2025-09-05 00:13:34 +1000

    leetcode/easy/2900.py

---
+++ leetcode/easy/2900.py
@@ -0,0 +1,71 @@
+"""
+2900. Longest Unequal Adjacent Groups Subsequence I
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a string array words and a binary array groups both of length n.
+
+A subsequence of words is alternating if for any two consecutive strings in the sequence, their corresponding elements at the same indices in groups are different (that is, there cannot be consecutive 0 or 1).
+
+Your task is to select the longest alternating subsequence from words.
+
+Return the selected subsequence. If there are multiple answers, return any of them.
+
+Note: The elements in words are distinct.
+
+ 
+
+Example 1:
+
+Input: words = ["e","a","b"], groups = [0,0,1]
+
+Output: ["e","b"]
+
+Explanation: A subsequence that can be selected is ["e","b"] because groups[0] != groups[2]. Another subsequence that can be selected is ["a","b"] because groups[1] != groups[2]. It can be demonstrated that the length of the longest subsequence of indices that satisfies the condition is 2.
+
+Example 2:
+
+Input: words = ["a","b","c","d"], groups = [1,0,1,1]
+
+Output: ["a","b","c"]
+
+Explanation: A subsequence that can be selected is ["a","b","c"] because groups[0] != groups[1] and groups[1] != groups[2]. Another subsequence that can be selected is ["a","b","d"] because groups[0] != groups[1] and groups[1] != groups[3]. It can be shown that the length of the longest subsequence of indices that satisfies the condition is 3.
+
+ 
+
+Constraints:
+
+1 <= n == words.length == groups.length <= 100
+1 <= words[i].length <= 10
+groups[i] is either 0 or 1.
+words consists of distinct strings.
+words[i] consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, words: List[str], groups: List[int]) -> List[str]:
+        ll = len(words)
+        result = [words[0]]
+        for ii in range(1, ll):
+            if groups[ii] != groups[ii-1]:
+                result.append(words[ii])
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
