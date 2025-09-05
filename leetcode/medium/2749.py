5eaa65b5ba5f0b0e0daad5b1c163fa609c46a8d6
Xizhu Hou <houxizhu@gmail.com>
2025-09-05 11:58:10 +1000

    daily, leetcode/medium/2749.py

---
+++ leetcode/medium/2749.py
@@ -0,0 +1,76 @@
+"""
+2749. Minimum Operations to Make the Integer Zero
+Medium
+Topics
+￼
+Companies
+Hint
+You are given two integers num1 and num2.
+
+In one operation, you can choose integer i in the range [0, 60] and subtract 2i + num2 from num1.
+
+Return the integer denoting the minimum number of operations needed to make num1 equal to 0.
+
+If it is impossible to make num1 equal to 0, return -1.
+
+ 
+
+Example 1:
+
+Input: num1 = 3, num2 = -2
+Output: 3
+Explanation: We can make 3 equal to 0 with the following operations:
+- We choose i = 2 and subtract 22 + (-2) from 3, 3 - (4 + (-2)) = 1.
+- We choose i = 2 and subtract 22 + (-2) from 1, 1 - (4 + (-2)) = -1.
+- We choose i = 0 and subtract 20 + (-2) from -1, (-1) - (1 + (-2)) = 0.
+It can be proven, that 3 is the minimum number of operations that we need to perform.
+Example 2:
+
+Input: num1 = 5, num2 = 7
+Output: -1
+Explanation: It can be proven, that it is impossible to make 5 equal to 0 with the given operation.
+ 
+
+Constraints:
+
+1 <= num1 <= 109
+-109 <= num2 <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, num1: int, num2: int) -> int:
+        for ii in range(1, 100):
+            num1 -= num2
+
+            ### 85 - 42 - 42 = 1, impossible
+            if num1 < ii:
+                return -1
+
+            count = 0
+            for jj in range(64):
+                if num1 & (1<<jj):
+                    count += 1
+
+            #print(ii, num1, count)
+
+            if count <= ii:
+                return ii
+
+        return -1
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
