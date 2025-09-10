65363d4dfbf0c1822eabba5a30be1f4dea9ef104
Xizhu Hou <houxizhu@gmail.com>
2025-09-11 00:38:44 +1000

    leetcode/easy/2928.py

---
+++ leetcode/easy/2928.py
@@ -0,0 +1,57 @@
+"""
+2928. Distribute Candies Among Children I
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given two positive integers n and limit.
+
+Return the total number of ways to distribute n candies among 3 children such that no child gets more than limit candies.
+
+ 
+
+Example 1:
+
+Input: n = 5, limit = 2
+Output: 3
+Explanation: There are 3 ways to distribute 5 candies such that no child gets more than 2 candies: (1, 2, 2), (2, 1, 2) and (2, 2, 1).
+Example 2:
+
+Input: n = 3, limit = 3
+Output: 10
+Explanation: There are 10 ways to distribute 3 candies such that no child gets more than 3 candies: (0, 0, 3), (0, 1, 2), (0, 2, 1), (0, 3, 0), (1, 0, 2), (1, 1, 1), (1, 2, 0), (2, 0, 1), (2, 1, 0) and (3, 0, 0).
+ 
+
+Constraints:
+
+1 <= n <= 50
+1 <= limit <= 50
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int, limit: int) -> int:
+        result = 0
+        for ii in range(min(limit, n)+1):
+            for jj in range(min(limit, n-ii)+1):
+                #print(ii,jj, n-ii-jj, result)
+                if n-ii-jj <= limit:
+                    result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
