61c5aa29bfac5ce326821e3e405d18771ec8b4bc
Xizhu Hou <houxizhu@gmail.com>
2025-09-11 10:54:24 +1000

    daily, leetcode/medium/2785.py

---
+++ leetcode/medium/2785.py
@@ -0,0 +1,76 @@
+"""
+2785. Sort Vowels in a String
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a 0-indexed string s, permute s to get a new string t such that:
+
+All consonants remain in their original places. More formally, if there is an index i with 0 <= i < s.length such that s[i] is a consonant, then t[i] = s[i].
+The vowels must be sorted in the nondecreasing order of their ASCII values. More formally, for pairs of indices i, j with 0 <= i < j < s.length such that s[i] and s[j] are vowels, then t[i] must not have a higher ASCII value than t[j].
+Return the resulting string.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in lowercase or uppercase. Consonants comprise all letters that are not vowels.
+
+ 
+
+Example 1:
+
+Input: s = "lEetcOde"
+Output: "lEOtcede"
+Explanation: 'E', 'O', and 'e' are the vowels in s; 'l', 't', 'c', and 'd' are all consonants. The vowels are sorted according to their ASCII values, and the consonants remain in the same places.
+Example 2:
+
+Input: s = "lYmpH"
+Output: "lYmpH"
+Explanation: There are no vowels in s (all characters in s are consonants), so we return "lYmpH".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists only of letters of the English alphabet in uppercase and lowercase.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> str:
+        ll = len(s)
+        vowels = "AEIOUaeiou"
+        dd = defaultdict(int)
+        for each in s:
+            if each in vowels:
+                dd[each] += 1
+        
+        result = [""]*ll
+        index = 0
+        #print(dd)
+        for ii in range(ll):
+            #print(ii, s[ii], index, vowels[index], dd[vowels[index]], result, dd)
+            if s[ii] in vowels:
+                while dd[vowels[index]] == 0 and index < 10:
+                    #print(index, vowels[index])
+                    index += 1
+                result[ii] = vowels[index]
+                dd[vowels[index]] -= 1
+            else:
+                result[ii] = s[ii]
+
+        return "".join(result)
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
