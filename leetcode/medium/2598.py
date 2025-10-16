8fd49aff22609780aa26845df4717b0ad7c01a43
Xizhu Hou <houxizhu@gmail.com>
2025-10-16 22:19:58 +1100

    leetcode/medium/2598.py

--- leetcode/medium/2598.py
+++ leetcode/medium/2598.py
@@ -1,5 +1,44 @@
 """
+2598. Smallest Missing Non-negative Integer After Operations
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given a 0-indexed integer array nums and an integer value.
 
+In one operation, you can add or subtract value from any element of nums.
+
+For example, if nums = [1,2,3] and value = 2, you can choose to subtract value from nums[0] to make nums = [-1,2,3].
+The MEX (minimum excluded) of an array is the smallest missing non-negative integer in it.
+
+For example, the MEX of [-1,2,3] is 0 while the MEX of [1,0,3] is 2.
+Return the maximum MEX of nums after applying the mentioned operation any number of times.
+
+ 
+
+Example 1:
+
+Input: nums = [1,-10,7,13,6,8], value = 5
+Output: 4
+Explanation: One can achieve this result by applying the following operations:
+- Add value to nums[1] twice to make nums = [1,0,7,13,6,8]
+- Subtract value from nums[2] once to make nums = [1,0,2,13,6,8]
+- Subtract value from nums[3] twice to make nums = [1,0,2,3,6,8]
+The MEX of nums is 4. It can be shown that 4 is the maximum MEX we can achieve.
+Example 2:
+
+Input: nums = [1,-10,7,13,6,8], value = 7
+Output: 2
+Explanation: One can achieve this result by applying the following operation:
+- subtract value from nums[2] once to make nums = [1,-10,0,13,6,8]
+The MEX of nums is 2. It can be shown that 2 is the maximum MEX we can achieve.
+ 
+
+Constraints:
+
+1 <= nums.length, value <= 105
+-109 <= nums[i] <= 109
 """
 
 import string
@@ -14,14 +53,21 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def leetcode(self, nums: List[int]) -> List[int]:
+    def leetcode(self, nums: List[int], value: int) -> int:
         ll = len(nums)
-        result = 0
+        dd = defaultdict(int)
+        for num in nums:
+            dd[num%value] += 1
 
-        for ii in range(ll):
-            pass
+        #print(dd)
+        
+        for ii in range(10**6):
+            if dd[ii%value] == 0:
+                return ii
+            if (dd[ii%value]-1)*value + ii%value < ii:
+                return ii
 
-        return result
+        return ll
 
 
 if __name__ == "__main__":
