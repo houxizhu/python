1858eed00d351b233f255ae390a435320758b2f4
Xizhu Hou <houxizhu@gmail.com>
2025-10-18 13:24:34 +1100

    leetcode/medium/3397.py

---
+++ leetcode/medium/3397.py
@@ -0,0 +1,79 @@
+"""
+3397. Maximum Number of Distinct Elements After Operations
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums and an integer k.
+
+You are allowed to perform the following operation on each element of the array at most once:
+
+Add an integer in the range [-k, k] to the element.
+Return the maximum possible number of distinct elements in nums after performing the operations.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,3,3,4], k = 2
+
+Output: 6
+
+Explanation:
+
+nums changes to [-1, 0, 1, 2, 3, 4] after performing operations on the first four elements.
+
+Example 2:
+
+Input: nums = [4,4,4,4], k = 1
+
+Output: 3
+
+Explanation:
+
+By adding -1 to nums[0] and 1 to nums[1], nums changes to [3, 5, 4, 4].
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+0 <= k <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int], k: int) -> int:
+        cnt = Counter(nums)
+        nums = list(set(nums))
+        nums.sort()
+        ll = len(nums)
+        start = nums[0]-k
+        result = 0
+        for num in nums:
+            #print(num, start, result, cnt[num])
+            if start > num+k:
+                continue
+            candidates = min(cnt[num], num+k-max(start, num-k)+1)
+            result += candidates
+            start = max(start, num-k)+candidates
+        
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
