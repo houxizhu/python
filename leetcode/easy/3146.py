af583c19b18a67e712a1e4492ec14821e3ed7d9b
Xizhu Hou <houxizhu@gmail.com>
2025-10-20 12:13:48 +1100

    leetcode/easy/3146.py

---
+++ leetcode/easy/3146.py
@@ -0,0 +1,82 @@
+"""
+3146. Permutation Difference between Two Strings
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given two strings s and t such that every character occurs at most once in s and t is a permutation of s.
+
+The permutation difference between s and t is defined as the sum of the absolute difference between the index of the occurrence of each character in s and the index of the occurrence of the same character in t.
+
+Return the permutation difference between s and t.
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "bac"
+
+Output: 2
+
+Explanation:
+
+For s = "abc" and t = "bac", the permutation difference of s and t is equal to the sum of:
+
+The absolute difference between the index of the occurrence of "a" in s and the index of the occurrence of "a" in t.
+The absolute difference between the index of the occurrence of "b" in s and the index of the occurrence of "b" in t.
+The absolute difference between the index of the occurrence of "c" in s and the index of the occurrence of "c" in t.
+That is, the permutation difference between s and t is equal to |0 - 1| + |1 - 0| + |2 - 2| = 2.
+
+Example 2:
+
+Input: s = "abcde", t = "edbac"
+
+Output: 12
+
+Explanation: The permutation difference between s and t is equal to |0 - 3| + |1 - 2| + |2 - 4| + |3 - 1| + |4 - 0| = 12.
+
+ 
+
+Constraints:
+
+1 <= s.length <= 26
+Each character occurs at most once in s.
+t is a permutation of s.
+s consists only of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str, t: str) -> int:
+        lists = list(s)
+        listt = list(t)
+        ll = len(s)
+        for ii in range(ll):
+            lists[ii] = [lists[ii], ii]
+            listt[ii] = [listt[ii], ii]
+        
+        lists.sort()
+        listt.sort()
+        
+        result = 0
+        for ii in range(ll):
+            result += abs(lists[ii][1]-listt[ii][1])
+        
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
