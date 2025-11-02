5d83b6f422a02eeed15215cf42b17dbc228b965a
Xizhu Hou <houxizhu@gmail.com>
2025-11-02 13:54:11 +1100

    leetcode/contest/20251102.weekly.474

---
+++ leetcode/contest/20251102.weekly.474/q3-.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        result = 0
+
+        return result
