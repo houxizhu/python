63e85fe832e05bb39d706207ab89db66b4238014
Xizhu Hou <houxizhu@gmail.com>
2025-11-03 11:28:39 +1100

    leetcode/easy/3226.py

---
+++ leetcode/easy/3226.py
@@ -0,0 +1,84 @@
+"""
+3226. Number of Bit Changes to Make Two Integers Equal
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given two positive integers n and k.
+
+You can choose any bit in the binary representation of n that is equal to 1 and change it to 0.
+
+Return the number of changes needed to make n equal to k. If it is impossible, return -1.
+
+ 
+
+Example 1:
+
+Input: n = 13, k = 4
+
+Output: 2
+
+Explanation:
+Initially, the binary representations of n and k are n = (1101)2 and k = (0100)2.
+We can change the first and fourth bits of n. The resulting integer is n = (0100)2 = k.
+
+Example 2:
+
+Input: n = 21, k = 21
+
+Output: 0
+
+Explanation:
+n and k are already equal, so no changes are needed.
+
+Example 3:
+
+Input: n = 14, k = 13
+
+Output: -1
+
+Explanation:
+It is not possible to make n equal to k.
+
+ 
+
+Constraints:
+
+1 <= n, k <= 106
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int, k: int) -> int:
+        result = 0
+        for ii in range(32):
+            bit = 1<<ii
+            bitn = bit&n
+            bitk = bit&k
+
+            if bitn == bitk:
+                pass
+            else:
+                if bitk:
+                    return -1
+                else:
+                    result += 1
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
