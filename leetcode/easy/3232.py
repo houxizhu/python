81b19874f74fa33ca5e576a68339837dcb06f346
Xizhu Hou <houxizhu@gmail.com>
2025-11-04 00:09:05 +1100

    leetcode/easy/3232.py

---
+++ leetcode/easy/3232.py
@@ -0,0 +1,82 @@
+"""
+3232. Find if Digit Game Can Be Won
+Solved
+Easy
+Topics
+￼
+Companies
+Hint
+You are given an array of positive integers nums.
+
+Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
+
+Return true if Alice can win this game, otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,10]
+
+Output: false
+
+Explanation:
+
+Alice cannot win by choosing either single-digit or double-digit numbers.
+
+Example 2:
+
+Input: nums = [1,2,3,4,5,14]
+
+Output: true
+
+Explanation:
+
+Alice can win by choosing single-digit numbers which have a sum equal to 15.
+
+Example 3:
+
+Input: nums = [5,5,5,25]
+
+Output: true
+
+Explanation:
+
+Alice can win by choosing double-digit numbers which have a sum equal to 25.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 99
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> bool:
+        one = 0
+        two = 0
+        for each in nums:
+            if each < 10:
+                one += each
+            else:
+                two += each
+        if one-two:
+            return True
+        return False
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
