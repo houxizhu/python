7a2b0bc5d09a398c9867b44b322c95274d248bee
Xizhu Hou <houxizhu@gmail.com>
2025-11-12 09:41:02 +1100

    leetcode/easy/3280.py

---
+++ leetcode/easy/3280.py
@@ -0,0 +1,69 @@
+"""
+3280. Convert Date to Binary
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+You are given a string date representing a Gregorian calendar date in the yyyy-mm-dd format.
+
+date can be written in its binary representation obtained by converting year, month, and day to their binary representations without any leading zeroes and writing them down in year-month-day format.
+
+Return the binary representation of date.
+
+ 
+
+Example 1:
+
+Input: date = "2080-02-29"
+
+Output: "100000100000-10-11101"
+
+Explanation:
+
+100000100000, 10, and 11101 are the binary representations of 2080, 02, and 29 respectively.
+
+Example 2:
+
+Input: date = "1900-01-01"
+
+Output: "11101101100-1-1"
+
+Explanation:
+
+11101101100, 1, and 1 are the binary representations of 1900, 1, and 1 respectively.
+
+ 
+
+Constraints:
+
+date.length == 10
+date[4] == date[7] == '-', and all other date[i]'s are digits.
+The input is generated such that date represents a valid Gregorian calendar date between Jan 1st, 1900 and Dec 31st, 2100 (both inclusive).
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, date: str) -> str:
+        year = f"{int(date[0:4]):b}"
+        month = f"{int(date[5:7]):b}"
+        day = f"{int(date[8:]):b}"
+        #print(year,month,day)
+
+        return f"{year}-{month}-{day}"
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
