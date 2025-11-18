e26117061cf0033b055630e9bc716f935da79888
Xizhu Hou <houxizhu@gmail.com>
2025-11-19 07:59:17 +1100

    leetcode/easy/3330.py

---
+++ leetcode/easy/3330.py
@@ -0,0 +1,83 @@
+"""
+3330. Find the Original Typed String I
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Alice is attempting to type a specific string on her computer. However, she tends to be clumsy and may press a key for too long, resulting in a character being typed multiple times.
+
+Although Alice tried to focus on her typing, she is aware that she may still have done this at most once.
+
+You are given a string word, which represents the final output displayed on Alice's screen.
+
+Return the total number of possible original strings that Alice might have intended to type.
+
+ 
+
+Example 1:
+
+Input: word = "abbcccc"
+
+Output: 5
+
+Explanation:
+
+The possible strings are: "abbcccc", "abbccc", "abbcc", "abbc", and "abcccc".
+
+Example 2:
+
+Input: word = "abcd"
+
+Output: 1
+
+Explanation:
+
+The only possible string is "abcd".
+
+Example 3:
+
+Input: word = "aaaa"
+
+Output: 4
+
+ 
+
+Constraints:
+
+1 <= word.length <= 100
+word consists only of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, word: str) -> int:
+        ll = len(word)
+        count = 1
+        result = 1
+        for ii in range(1, ll):
+            if word[ii] == word[ii-1]:
+                count += 1
+            else:
+                result += count-1
+                count = 1
+        
+        result += count-1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
