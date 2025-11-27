ddb2bc6d9d93e0ed71f0449252bca2ede7af58d7
Xizhu Hou <houxizhu@gmail.com>
2025-11-27 15:09:38 +1100

    leetcode/medium/3381.py

---
+++ leetcode/medium/3381.py
@@ -0,0 +1,72 @@
+"""
+3381. Maximum Subarray Sum With Length Divisible by K
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an array of integers nums and an integer k.
+
+Return the maximum sum of a subarray of nums, such that the size of the subarray is divisible by k.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2], k = 1
+
+Output: 3
+
+Explanation:
+
+The subarray [1, 2] with sum 3 has length equal to 2 which is divisible by 1.
+
+Example 2:
+
+Input: nums = [-1,-2,-3,-4,-5], k = 4
+
+Output: -10
+
+Explanation:
+
+The maximum sum subarray is [-1, -2, -3, -4] which has length equal to 4 which is divisible by 4.
+
+Example 3:
+
+Input: nums = [-5,1,2,-3,4], k = 2
+
+Output: 4
+
+Explanation:
+
+The maximum sum subarray is [1, 2, -3, 4] which has length equal to 4 which is divisible by 2.
+
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 2 * 105
+-109 <= nums[i] <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def leetcode(self, nums: List[int], k: int) -> int:
+        ll = len(nums)
+        prefix_sum = 0
+
+        result = sum(nums[:k])
+        min_prefix_sum = [float("inf")]*k
+        min_prefix_sum[0] = 0
+
+        for ii in range(ll):
+            prefix_sum += nums[ii]
+            mod = (ii+1)%k
+            result = max(result, prefix_sum - min_prefix_sum[mod])
+            min_prefix_sum[mod] = min(min_prefix_sum[mod], prefix_sum)
+
+        return result
