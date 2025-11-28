5f80ae8419789f9a19008566f850b4188d092947
Xizhu Hou <houxizhu@gmail.com>
2025-11-29 00:16:18 +1100

    leetcode/easy/3379.py

---
+++ leetcode/easy/3379.py
@@ -0,0 +1,77 @@
+"""
+3379. Transformed Array
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums that represents a circular array. Your task is to create a new array result of the same size, following these rules:
+
+For each index i (where 0 <= i < nums.length), perform the following independent actions:
+If nums[i] > 0: Start at index i and move nums[i] steps to the right in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] < 0: Start at index i and move abs(nums[i]) steps to the left in the circular array. Set result[i] to the value of the index where you land.
+If nums[i] == 0: Set result[i] to nums[i].
+Return the new array result.
+
+Note: Since nums is circular, moving past the last element wraps around to the beginning, and moving before the first element wraps back to the end.
+
+ 
+
+Example 1:
+
+Input: nums = [3,-2,1,1]
+
+Output: [1,1,1,3]
+
+Explanation:
+
+For nums[0] that is equal to 3, If we move 3 steps to right, we reach nums[3]. So result[0] should be 1.
+For nums[1] that is equal to -2, If we move 2 steps to left, we reach nums[3]. So result[1] should be 1.
+For nums[2] that is equal to 1, If we move 1 step to right, we reach nums[3]. So result[2] should be 1.
+For nums[3] that is equal to 1, If we move 1 step to right, we reach nums[0]. So result[3] should be 3.
+Example 2:
+
+Input: nums = [-1,4,-1]
+
+Output: [-1,-1,4]
+
+Explanation:
+
+For nums[0] that is equal to -1, If we move 1 step to left, we reach nums[2]. So result[0] should be -1.
+For nums[1] that is equal to 4, If we move 4 steps to right, we reach nums[2]. So result[1] should be -1.
+For nums[2] that is equal to -1, If we move 1 step to left, we reach nums[1]. So result[2] should be 4.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> List[int]:
+        ll = len(nums)
+        result = [0]*ll
+        for ii in range(ll):
+            if nums[ii] == 0:
+                result[ii] = nums[ii]
+            else:
+                result[ii] = nums[(ii+nums[ii])%ll]
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
