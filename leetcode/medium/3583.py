c80083241b0c4951134f967dd8013bd1d8d4706b
Xizhu Hou <houxizhu@gmail.com>
2025-12-09 22:07:37 +1100

    leetcode/medium/3583.py

---
+++ leetcode/medium/3583.py
@@ -0,0 +1,100 @@
+"""
+3583. Count Special Triplets
+Attempted
+Medium
+Topics
+￼
+Companies
+Hint
+You are given an integer array nums.
+
+A special triplet is defined as a triplet of indices (i, j, k) such that:
+
+0 <= i < j < k < n, where n = nums.length
+nums[i] == nums[j] * 2
+nums[k] == nums[j] * 2
+Return the total number of special triplets in the array.
+
+Since the answer may be large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: nums = [6,3,6]
+
+Output: 1
+
+Explanation:
+
+The only special triplet is (i, j, k) = (0, 1, 2), where:
+
+nums[0] = 6, nums[1] = 3, nums[2] = 6
+nums[0] = nums[1] * 2 = 3 * 2 = 6
+nums[2] = nums[1] * 2 = 3 * 2 = 6
+Example 2:
+
+Input: nums = [0,1,0,0]
+
+Output: 1
+
+Explanation:
+
+The only special triplet is (i, j, k) = (0, 2, 3), where:
+
+nums[0] = 0, nums[2] = 0, nums[3] = 0
+nums[0] = nums[2] * 2 = 0 * 2 = 0
+nums[3] = nums[2] * 2 = 0 * 2 = 0
+Example 3:
+
+Input: nums = [8,4,2,8,4]
+
+Output: 2
+
+Explanation:
+
+There are exactly two special triplets:
+
+(i, j, k) = (0, 1, 3)
+nums[0] = 8, nums[1] = 4, nums[3] = 8
+nums[0] = nums[1] * 2 = 4 * 2 = 8
+nums[3] = nums[1] * 2 = 4 * 2 = 8
+(i, j, k) = (1, 2, 4)
+nums[1] = 4, nums[2] = 2, nums[4] = 4
+nums[1] = nums[2] * 2 = 2 * 2 = 4
+nums[4] = nums[2] * 2 = 2 * 2 = 4
+ 
+
+Constraints:
+
+3 <= n == nums.length <= 105
+0 <= nums[i] <= 105
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        dd = defaultdict(int)
+        ii_before_jj = [0]*ll
+        for ii in range(ll):
+            ### step 2
+            dd[nums[ii]] += 1
+            ### step 3
+            ii_before_jj[ii] = dd[nums[ii]*2]
+
+        result = 0
+        ### step 5, special case of 0
+        if dd[0] > 2:
+            result = dd[0]*(dd[0]-1)*(dd[0]-2)//6
+
+        for jj in range(1, ll-1):
+            if nums[jj] == 0:
+                continue
+            result += ii_before_jj[jj]*(dd[nums[jj]*2] - ii_before_jj[jj])
+        
+        return result%(10**9+7)
