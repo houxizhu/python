d647da60bb95a4af74fa359cb193eec842f8636c
Xizhu Hou <houxizhu@gmail.com>
2025-12-16 00:11:54 +1100

    leetcode/easy/3471.py

---
+++ leetcode/easy/3471.py
@@ -0,0 +1,109 @@
+"""
+3471. Find the Largest Almost Missing Integer
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums and an integer k.
+
+An integer x is almost missing from nums if x appears in exactly one subarray of size k within nums.
+
+Return the largest almost missing integer from nums. If no such integer exists, return -1.
+
+A subarray is a contiguous sequence of elements within an array.
+ 
+
+Example 1:
+
+Input: nums = [3,9,2,1,7], k = 3
+
+Output: 7
+
+Explanation:
+
+1 appears in 2 subarrays of size 3: [9, 2, 1] and [2, 1, 7].
+2 appears in 3 subarrays of size 3: [3, 9, 2], [9, 2, 1], [2, 1, 7].
+3 appears in 1 subarray of size 3: [3, 9, 2].
+7 appears in 1 subarray of size 3: [2, 1, 7].
+9 appears in 2 subarrays of size 3: [3, 9, 2], and [9, 2, 1].
+We return 7 since it is the largest integer that appears in exactly one subarray of size k.
+
+Example 2:
+
+Input: nums = [3,9,7,2,1,7], k = 4
+
+Output: 3
+
+Explanation:
+
+1 appears in 2 subarrays of size 4: [9, 7, 2, 1], [7, 2, 1, 7].
+2 appears in 3 subarrays of size 4: [3, 9, 7, 2], [9, 7, 2, 1], [7, 2, 1, 7].
+3 appears in 1 subarray of size 4: [3, 9, 7, 2].
+7 appears in 3 subarrays of size 4: [3, 9, 7, 2], [9, 7, 2, 1], [7, 2, 1, 7].
+9 appears in 2 subarrays of size 4: [3, 9, 7, 2], [9, 7, 2, 1].
+We return 3 since it is the largest and only integer that appears in exactly one subarray of size k.
+
+Example 3:
+
+Input: nums = [0,0], k = 1
+
+Output: -1
+
+Explanation:
+
+There is no integer that appears in only one subarray of size 1.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+0 <= nums[i] <= 50
+1 <= k <= nums.length
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int], k: int) -> int:
+        ll = len(nums)
+        result = -1
+        if ll == 1:
+            return nums[0]
+        if ll == k:
+            return max(nums)
+            
+        dd = defaultdict(int)
+        for ii in range(ll):
+            dd[nums[ii]] += 1
+            
+        if k == 1:
+            for each in dd:
+                if dd[each] == 1:
+                    result = max(result, each)
+        else:
+            if nums[0] == nums[-1]:
+                return -1
+            else:
+                if dd[nums[0]] == 1:
+                    result = max(result, nums[0])
+                if dd[nums[-1]] == 1:
+                    result = max(result, nums[-1])
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
