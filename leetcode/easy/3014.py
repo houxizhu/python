c07715c52ed8b2134fbc25dc6b5693bf8123326b
Xizhu Hou <houxizhu@gmail.com>
2025-12-17 00:09:35 +1100

    leetcode/easy/3014.py

---
+++ leetcode/easy/3014.py
@@ -0,0 +1,91 @@
+"""
+3014. Minimum Number of Pushes to Type Word I
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given a string word containing distinct lowercase English letters.
+
+Telephone keypads have keys mapped with distinct collections of lowercase English letters, which can be used to form words by pushing them. For example, the key 2 is mapped with ["a","b","c"], we need to push the key one time to type "a", two times to type "b", and three times to type "c" .
+
+It is allowed to remap the keys numbered 2 to 9 to distinct collections of letters. The keys can be remapped to any amount of letters, but each letter must be mapped to exactly one key. You need to find the minimum number of times the keys will be pushed to type the string word.
+
+Return the minimum number of pushes needed to type word after remapping the keys.
+
+An example mapping of letters to keys on a telephone keypad is given below. Note that 1, *, #, and 0 do not map to any letters.
+
+
+ 
+
+Example 1:
+
+
+Input: word = "abcde"
+Output: 5
+Explanation: The remapped keypad given in the image provides the minimum cost.
+"a" -> one push on key 2
+"b" -> one push on key 3
+"c" -> one push on key 4
+"d" -> one push on key 5
+"e" -> one push on key 6
+Total cost is 1 + 1 + 1 + 1 + 1 = 5.
+It can be shown that no other mapping can provide a lower cost.
+Example 2:
+
+
+Input: word = "xycdefghij"
+Output: 12
+Explanation: The remapped keypad given in the image provides the minimum cost.
+"x" -> one push on key 2
+"y" -> two pushes on key 2
+"c" -> one push on key 3
+"d" -> two pushes on key 3
+"e" -> one push on key 4
+"f" -> one push on key 5
+"g" -> one push on key 6
+"h" -> one push on key 7
+"i" -> one push on key 8
+"j" -> one push on key 9
+Total cost is 1 + 2 + 1 + 2 + 1 + 1 + 1 + 1 + 1 + 1 = 12.
+It can be shown that no other mapping can provide a lower cost.
+ 
+
+Constraints:
+
+1 <= word.length <= 26
+word consists of lowercase English letters.
+All letters in word are distinct.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, word: str) -> int:
+        count = [0]*33
+        for each in word:
+            count[ord(each)-ord("a")] += 1
+        count.sort(reverse=True)
+        result = 0
+        for ii in range(8):
+            result += count[ii]
+            result += count[ii+8]*2
+            result += count[ii+16]*3
+            result += count[ii+24]*4
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
