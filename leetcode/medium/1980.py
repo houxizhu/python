af4b0e0c645a96933e444109f7d63fd26b7ee343
Xizhu Hou <houxizhu@gmail.com>
2026-03-08 23:33:45 +1100

    leetcode/medium/1980.py

---
+++ leetcode/medium/1980.py
@@ -0,0 +1,51 @@
+"""
+1980. Find Unique Binary String
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
+
+ 
+
+Example 1:
+
+Input: nums = ["01","10"]
+Output: "11"
+Explanation: "11" does not appear in nums. "00" would also be correct.
+Example 2:
+
+Input: nums = ["00","01"]
+Output: "11"
+Explanation: "11" does not appear in nums. "10" would also be correct.
+Example 3:
+
+Input: nums = ["111","011","001"]
+Output: "101"
+Explanation: "101" does not appear in nums. "000", "010", "100", and "110" would also be correct.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 16
+nums[i].length == n
+nums[i] is either '0' or '1'.
+All the strings of nums are unique.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def leetcode(self, nums: List[str]) -> str:
+        n = len(nums)
+        num17 = ["00000", "00001", "00010", "00011", "00100", "00101", "00110", "00111", "01000", "01001", "01010", "01011", "01100", "01101", "01110", "01111", "10000"]
+
+        for num in num17:
+            if ("00000000000"+num)[-n:] not in nums:
+                return ("00000000000"+num)[-n:]
